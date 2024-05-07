5c64d6a52d2d972b606a81e55ffbfbcc386fc6ac
3phraimY <ephraim.young@cuw.edu>
2024-05-07 14:01:42 -0500

    added comments

--- Main.py
+++ Main.py
@@ -1,5 +1,11 @@
 from howlongtobeatpy  import HowLongToBeat
 
+#seach from ID for HowLongToBeatEntry
 result = HowLongToBeat().search_from_id(7231)
+
+#access attribute from json dictionary
 print(result.json_content['game_name'])
+
+#access attribute directly
 print(result.game_name)
+
